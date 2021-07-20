f028795c48ef72f17715de8f20e18bfe72659920
PiotrWR <pwrynio@gmail.com>
2021-07-21 00:24:15 +0200

    Add empty molecules

---
+++ src/components/molecules/NewMessageTextInput/NewMessageTextInput.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "NewMessageTextInput"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/molecules/NewMessageTitleInput/NewMessageTitleInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>input</div>
+</template>
+
+<script>
+export default {
+  name: "NewMessageTitleInput"
+}
+</script>
+
+<style scoped>
+
+</style>
