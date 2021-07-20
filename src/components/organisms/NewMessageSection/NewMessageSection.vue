cc69285de06525ca8c2e48feda75ec1bd2ee6163
PiotrWR <pwrynio@gmail.com>
2021-07-20 23:56:15 +0200

    Add empty NewMessageSection.vue

---
+++ src/components/organisms/NewMessageSection/NewMessageSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="newMessageSection">
+    <SectionTitle/>
+  </section>
+</template>
+
+<script>
+import SectionTitle from '../../atoms/SectionTitle/SectionTitle.vue'
+
+export default {
+  name: "NewMessageSection",
+  components: {SectionTitle}
+}
+</script>
+
+<style scoped lang="scss">
+.newMessageSection {
+  display: flex;
+  width: 100%;
+  max-width: 1000px;
+  padding: 24px 20px 0;
+  flex-flow: column nowrap;
+  background-color: rgba(100, 100, 100, .2);
+
+  @media (min-width: $md-width) {
+    padding: 32px 0 0;
+  }
+}
+</style>
--- src/components/pages/RickAndMorty/RickAndMorty.vue
+++ src/components/pages/RickAndMorty/RickAndMorty.vue
@@ -1,19 +1,35 @@
 <template>
-  <div>
+  <div class="rickAndMorty">
     <GlobalHeaderBar/>
-    <div>main section</div>
+    <NewMessageSection class="rickAndMorty__section"/>
   </div>
 </template>
 
 <script>
 import GlobalHeaderBar from '../../organisms/GlobalHeaderBar/GlobalHeaderBar.vue'
+import NewMessageSection from '../../organisms/NewMessageSection/NewMessageSection'
 
 export default {
   name: "RickAndMorty",
-  components: {GlobalHeaderBar,}
+  components: {
+    GlobalHeaderBar,
+    NewMessageSection
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.rickAndMorty {
+  &__section {
+    margin: $globalHeaderBar-height-sm auto 0;
+  }
+}
 
+@media (min-width: $md-width) {
+  .rickAndMorty {
+    &__section {
+      margin-top: $globalHeaderBar-height-md;
+    }
+  }
+}
 </style>
