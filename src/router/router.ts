7e3433f4216ca1570842b25ce8caa96f59736107
PiotrWR <pwrynio@gmail.com>
2021-07-22 16:01:26 +0200

    Add empty MessageHistorySection.vue

---
+++ src/router/router.ts
@@ -0,0 +1,23 @@
+// import { createWebHistory, createRouter } from "vue-router";
+// import NewMessageSection from "@/components/organisms/NewMessageSection/NewMessageSection.vue";
+// import MessageHistorySection from "@/components/organisms/MessageHistorySection/MessageHistorySection.vue";
+//
+// const routes = [
+//    {
+//       path: "/",
+//       name: "home",
+//       component: NewMessageSection,
+//    },   {
+//       path: "/history",
+//       name: "history",
+//       component: MessageHistorySection,
+//    },
+// ];
+//
+// const router = createRouter({
+//    history: createWebHistory(),
+//    routes,
+// });
+//
+// export default router;
+//
